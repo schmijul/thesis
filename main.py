4ef567b555e654dd06b6305c711d40d5a7e85ecf
JulianSchmidt96 <julianschmidtbewerbung@gmail.com>
2022-06-24 10:44:39 +0200

    typo fix

--- main.py
+++ main.py
@@ -381,9 +381,10 @@ if __name__ == '__main__':
             reduceResolution = 0
                 
         
-        for samplingDistance_x in [ 42, 40,38, 36, 34, 32, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10, 8, 6, 4, 2]: ]: 
+        for samplingDistance_x in [ 42, 40,38, 36, 34, 32, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10, 8, 6, 4, 2]: 
                 
                 samplingDistance_y = samplingDistance_x * 12
+                
                 for random in [0,1]:
                     if random:
                             
